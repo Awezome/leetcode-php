b4fbb4fd3c095120ca94d63853571497bdb4820b
Awezome <likezyp@gmail.com>
2019-04-14 19:23:51 +0800

    update

--- src/动态规划/70. 爬楼梯.php
+++ src/动态规划/70. 爬楼梯.php
@@ -5,39 +5,9 @@
  * Time: 16:10
  */
 
-/*********  普通的递归过程：LeetCode无法通过  *********/
-class Solution {
-    /**
-     * @param Integer $n
-     * @return Integer
-     */
-    function climbStairs($n) {
-        return $this->calway($n);
-    }
-    private function calway($n){
-        if($n == 0 || $n ==1) return 1;     //层层递归过程
-        return $this->calway($n-1) + $this->calway($n-2);
-    }
-}
-/*********  记忆化搜索：LeetCode 16ms  *********/
-class Solution1 {
-    private $memory = [];
-    /**
-     * @param Integer $n
-     * @return Integer
-     */
-    function climbStairs($n) {
-        return $this->calway($n);
-    }
-    private function calway($n){
-        if($n == 0 || $n ==1) return 1;
-        if(empty($this->memory[$n]))
-            $this->memory[$n] = $this->calway($n-1) + $this->calway($n-2);
-        return $this->memory[$n];
-    }
-}
+
 /*********  动态规划：LeetCode 12ms  *********/
-class Solution2 {
+class Solution {
     /**
      * @param Integer $n
      * @return Integer
